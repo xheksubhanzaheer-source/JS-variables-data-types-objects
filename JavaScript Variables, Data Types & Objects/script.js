8255aba3e534fd6b5b7b3d6a833abcc75fcdfc26
Subhan Zaheer <xheksubhan.zaheer@gmail.com>
2026-01-04 03:15:24 +0500

    Add files via upload

---
+++ JavaScript Variables, Data Types & Objects/script.js
@@ -0,0 +1,45 @@
+  
+
+console.log("hey this is the tutorial 55")
+
+let a = 5;
+// a = a + 1
+let b = 6;
+let c = "Harry"
+// let 55a = "Hey"-------not allowed
+
+// console.log(a+b+8)
+
+//  for finding data type of variables use this
+// console.log(typeof a, typeof b, typeof c)
+{
+    let a = 66;
+    console.log(a)
+}
+console.log(a)
+
+const a1 = 2;
+//  the value of a1 cannot change because now this is constant
+a = a1 + 5;
+
+let x = "Subhan";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    "name": "Sheikh",
+    "job code": 5600,
+    "is_handsome": true
+}
+
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crores";
+console.log(o);
